7dd9c9f1f495378ee3b07e5e81849688ee4258ca
Ahmed Radi <20713411+0xRadi@users.noreply.github.com>
2020-06-11 13:02:47 +0800

    configured admin

--- jitsi/admin.py
+++ jitsi/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from jitsi.models import Meeting, Attendee
 
-# Register your models here.
+
+class AttendeeInline(admin.TabularInline):
+    model = Attendee
+
+
+class MeetingAdmin(admin.ModelAdmin):
+    inlines = [AttendeeInline]
+    list_display = ('room_id', 'subject', 'start_time', 'owner', 'is_anonymous')
+    list_filter = ['start_time']
+    search_fields = ['subject']
+
+
+admin.site.register(Meeting, MeetingAdmin)
